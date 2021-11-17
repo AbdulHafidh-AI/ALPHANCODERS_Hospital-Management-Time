d3b5c998733c6d517211cef3798d839c00be601a
ryeind27 <91448000+ryeind27@users.noreply.github.com>
2021-11-17 21:44:37 +0700

    Update RumahSakit
    menambahkan method yang diperlukan pada class RumahSakit

--- src/RumahSakit.java
+++ src/RumahSakit.java
@@ -4,4 +4,30 @@
  */
 public class RumahSakit 
 {
+     //Field
+     private String nama;
+     private String kota;
+     /**
+      * method constructor di class RumahSakit
+      */
+     public RumahSakit()
+     {
+          this.nama = "Alphan Hospital";
+     }
+     /**
+      * Method Setter untuk mengset Kota yang di inginkan
+      * @param kota
+      */
+     public void setKota(String kota)
+     {
+          this.kota = kota;
+     }
+     /**
+      * sebuat Method getter untuk mendapatkan nilai dari variable kota
+      * @return this.kota
+      */
+     public String getKota()
+     {
+          return this.kota;
+     }
 }
