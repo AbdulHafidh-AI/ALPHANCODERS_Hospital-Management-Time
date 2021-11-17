4a6b996b5fdfdd5914f109bbd290ab57b3632bd3
M. Khawaril Putra <m.khawarilputra24@gmail.com>
2021-11-17 22:09:27 +0700

    Menambahkan Method pada Pasien.java

--- src/Pasien.java
+++ src/Pasien.java
@@ -4,4 +4,49 @@
  */
 public class Pasien 
 {
+    private int jumlahPasien;
+    private int jamKerjaMinimal;
+
+    public Pasien(){
+
+    }
+
+    /**
+     * Untuk menentukan jumlah pasien agar mudah mengatur jadwal
+     * @param jumlahPasien dari hasil random
+     */
+    public void setJumlahPasien(int jumlahPasien){
+        this.jumlahPasien = jumlahPasien;
+    }
+
+    /**
+     * Untuk menentukan jumlah jam kerja minimal perhari berdasarkan jumlah pasien
+     */
+    public void setJamKerjaMinimal(){
+
+
+        if (this.jumlahPasien > 0 && this.jumlahPasien < 30) {
+            this.jamKerjaMinimal = 3;
+        }
+        else if (this.jumlahPasien >= 30 && this.jumlahPasien < 50) {
+            this.jamKerjaMinimal = 5;
+        }
+        else if (this.jumlahPasien >= 50 && this.jumlahPasien < 80) {
+            this.jamKerjaMinimal = 8;
+        }
+        else if (this.jumlahPasien >= 50 && this.jumlahPasien <= 100) {
+            this.jamKerjaMinimal = 12;
+        }
+        else {
+        
+        }
+    }
+
+    /**
+     * 
+     * @return jumlah jam kerja minimal
+     */
+    public int getJamKerjaMinimal(){
+        return this.jamKerjaMinimal;
+    }
 }
