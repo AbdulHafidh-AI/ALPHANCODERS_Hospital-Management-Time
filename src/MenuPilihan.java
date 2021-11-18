0797e1f67b43a63438651cd04652837b4796b9dc
Nurfajriati <93395295+Nurfa49@users.noreply.github.com>
2021-11-18 20:21:32 +0700

    update
    Menambahkan satu file dan mengisinya serta menghapus isi file perawat yang tidak penting

---
+++ src/MenuPilihan.java
@@ -0,0 +1,33 @@
+/**
+ * Sebuah class untuk memilih dalam registrasi profesi di Rumah Sakit Alphan
+ * @author Alphancoders
+ * @version 2021.11.18
+ */
+public class MenuPilihan 
+{
+    //fields 
+    private int pilihan;
+    /**
+     * Sebuah method constructor pada class ini
+     */
+    public MenuPilihan()
+    {
+
+    }
+    /**
+     * Sebuah method setter untuk mengeset variabel pilihan
+     * @param pilihan
+     */
+    public void setPilihan(int pilihan)
+    {
+        this.pilihan = pilihan;
+    }
+    /**
+     * Sebuah method getter untuk mendapatkan nilai dari variabel pilihan
+     * @return this.pilihan
+     */
+    public int getPilihan()
+    {
+        return this.pilihan;
+    }
+}
