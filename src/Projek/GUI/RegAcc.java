68ee375ad7e4dd7217593c5dc32edb37165ac8dc
ryeind27 <91448000+ryeind27@users.noreply.github.com>
2021-11-20 22:19:42 +0700

    added GUI form
    menambahkan beberapa fitur ke form GUI

--- src/Projek/GUI/RegAcc.java
+++ src/Projek/GUI/RegAcc.java
@@ -1,4 +1,26 @@
 package Projek.GUI;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class RegAcc {
+    private JPanel LoginPage;
+    private JButton PEGAWAIButton;
+    private JButton PERAWATButton;
+    private JButton DOKTERButton;
+
+    public RegAcc() {
+        DOKTERButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
