76c366edbd7bd33da35f81aac40712ec3ce1e914
ryeind27 <91448000+ryeind27@users.noreply.github.com>
2021-11-20 16:44:29 +0700

    Create class RegPekerjaHospital
    create class and import some java,io & java.util

---
+++ src/Projek/MAIN/RegPekerjaHospital.java
@@ -0,0 +1,17 @@
+package Projek.MAIN;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+
+public class RegPekerjaHospital 
+{
+
+}
