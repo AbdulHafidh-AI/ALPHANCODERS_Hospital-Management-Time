78518ca199ad848347fefc3d31f2c3378f7f6f90
ryeind27 <91448000+ryeind27@users.noreply.github.com>
2021-11-20 23:58:20 +0700

    Update

--- src/Projek/MAIN/RegPekerjaHospital.java
+++ src/Projek/MAIN/RegPekerjaHospital.java
@@ -1,16 +1,5 @@
 package Projek.MAIN;
 
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Scanner;
-
-
 public class RegPekerjaHospital 
 {
 
